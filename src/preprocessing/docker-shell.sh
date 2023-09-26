c06ce10a25150a97fc026c9283586c49d9e529a3
jeremyzhang1 <zhang.jeremy.2001@gmail.com>
2023-09-26 17:46:16 -0400

    fix secrets directory for preprocessing

--- src/preprocessing/docker-shell.sh
+++ src/preprocessing/docker-shell.sh
@@ -6,10 +6,13 @@ set -e
 # Define some environment variables
 export IMAGE_NAME="mockmate-data-preprocess"
 export BASE_DIR="$(pwd)"
+export SECRETS_DIR="$(pwd)"/../secrets/
 
 # Build the image based on the Dockerfile
 docker build -t $IMAGE_NAME -f Dockerfile .
 
 # Run the container
 docker run --rm --name $IMAGE_NAME -ti \
+-v "$SECRETS_DIR":/secrets \
+-e GOOGLE_APPLICATION_CREDENTIALS=/secrets/data-service-account.json \
 --mount type=bind,source="$BASE_DIR",target=/app $IMAGE_NAME
