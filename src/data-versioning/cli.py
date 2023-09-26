f40512ab01616e67640d875e231b4d5e8b63eb09
bbytiger <andy200212@gmail.com>
2023-09-26 13:17:41 -0400

    think script is fixed, going to do some tests now

--- src/data-versioning/cli.py
+++ src/data-versioning/cli.py
@@ -2,35 +2,34 @@
 Module that contains the command line app.
 """
 import argparse
-import os
-import traceback
-import time
-from google.cloud import storage
-import shutil
 import glob
 import json
+import os
+import pickle
+import shutil
+
+from google.cloud import storage
+from sentence_transformers import SentenceTransformer
 
 
-GCS_BUCKET_NAME = os.environ["GCS_BUCKET_NAME"]
-
-
-def make_local_dataset_folders(dataset_prep_folder):
+def make_local_dataset_folders(dataset_prep_folder, dataset_result_folder):
     print("cleaning and remaking local dataset folders")
     
     shutil.rmtree(dataset_prep_folder, ignore_errors=True, onerror=None)
     os.makedirs(dataset_prep_folder, exist_ok=True)
-    dataset_folder = "leetcode_dataset"
-    shutil.rmtree(dataset_folder, ignore_errors=True, onerror=None)
-    os.makedirs(dataset_folder, exist_ok=True)
+    shutil.rmtree(dataset_result_folder, ignore_errors=True, onerror=None)
+    os.makedirs(dataset_result_folder, exist_ok=True)
 
 
 def download_processed_leetcode_data(bucket_name, dataset_prep_folder):
     print("downloading processed leetcode data")
+
+    # init storage client
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blobs = bucket.list_blobs(prefix="processed_leetcode_data/")
 
-    # Download annotations
+    # download annotations
     for blob in blobs:
         print("Annotation file:", blob.name)
 
@@ -40,48 +39,46 @@ def download_processed_leetcode_data(bucket_name, dataset_prep_folder):
             blob.download_to_filename(local_file_path)
 
 
+def create_and_save_vector_embeddings(file):
+    # read the json file
+    with open(file, "r") as read_file:
+        leetcode_json = json.load(read_file)
+
+        problems = []
+        solutions = []
+
+        for problem in leetcode_json:
+            problem_dict = {key: problem[key] for key in []}
+            solution_dict = {key: problem[key] for key in []}
+            problems.append(json.dumps(problem_dict))
+            solutions.append(json.dumps(solution_dict))
+
+        problem_embeddings = embedding_model.encode(problems)
+        solution_embeddings = embedding_model.encode(solutions)
+
+        with open(dataset_result_folder + '/problems.pkl', 'wb') as handle:
+            pickle.dump(problem_embeddings,
+                        handle,
+                        protocol=pickle.HIGHEST_PROTOCOL)
+            
+        with open(dataset_result_folder + '/solutions.pkl', 'wb') as handle:
+            pickle.dump(solution_embeddings,
+                        handle,
+                        protocol=pickle.HIGHEST_PROTOCOL)
+
+
 def download_data():
-    print("download_data")
-
-    bucket_name = GCS_BUCKET_NAME
-    dataset_prep_folder = "leetcode_dataset_prep"
-
-    print("bucket_name:", bucket_name,
-          "dataset_prep_folder:", dataset_prep_folder)
-
-    # Clear dataset folders
+    # clear dataset folders
     make_local_dataset_folders(dataset_prep_folder)
 
-    # Initiate Storage client and download processed leetcode data
+    # initiate Storage client and download processed leetcode data
     download_processed_leetcode_data(bucket_name, dataset_prep_folder)
 
-    # Organize annotation with images
+    # organize annotation with images
     annotation_files = glob.glob(os.path.join(dataset_prep_folder, "*"))
     for annotation_file in annotation_files:
-        # Read the json file
-        with open(annotation_file, "r") as read_file:
-            annotation_json = json.load(read_file)
-
-        # Annotations
-        # annotations = annotation_json["annotations"]
-        # Assume we pick just the first annotation from the labeled list
-        if len(annotation_json["result"]) > 0:
-            label = annotation_json["result"][0]["value"]["choices"][0]
-            # Create the label folder
-            label_folder = os.path.join(dataset_folder, label)
-            os.makedirs(label_folder, exist_ok=True)
-
-            # Download the image from GCS [Another option could be to just store the image url and label in DVC]
-            image_url = annotation_json["task"]["data"]["image"]
-            image_url = image_url.replace("gs://", "").replace(
-                GCS_BUCKET_NAME + "/", ""
-            )
-            print("image_url:", image_url)
-            blob = bucket.blob(image_url)
-            filename = os.path.basename(blob.name)
-            local_file_path = os.path.join(label_folder, filename)
-            blob.download_to_filename(local_file_path)
-
+        create_and_save_vector_embeddings(annotation_file)
+        
 
 def main(args=None):
     if args.download:
@@ -89,8 +86,18 @@ def main(args=None):
 
 
 if __name__ == "__main__":
-    # Generate the inputs arguments parser
-    # if you type into the terminal 'python cli.py --help', it will provide the description
+    # init environment objects
+    bucket_name = os.environ["GCS_BUCKET_NAME"]
+    dataset_prep_folder = "leetcode_dataset_prep"
+    dataset_result_folder = "leetcode_dataset_embeddings"
+    print("bucket_name:", bucket_name,
+          "dataset_prep_folder:", dataset_prep_folder)
+
+    # init model object
+    embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    # generate the inputs arguments parser
+    # if you type 'python cli.py --help', it will provide the description
     parser = argparse.ArgumentParser(description="Data Versioning CLI...")
 
     parser.add_argument(
