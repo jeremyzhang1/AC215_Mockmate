1eac7f0badec79385ac76dc1a3a3abbc951a65ee
bbytiger <andy200212@gmail.com>
2023-10-03 16:08:55 -0400

    save work

---
+++ src/model/finetune.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+def create_model_checkpoint():
+    """
+    Create a checkpoint for the model.
+
+    Returns:
+        None
+    """
+    pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Model Fine-tuning CLI...")
---
+++ src/model/loader.py
@@ -0,0 +1,46 @@
+import argparse
+from transformers import AutoTokenizer
+
+model_name = "tiiuae/falcon-7b"  # hardcode the model name for now
+model_dir_name = "falcon7b"
+
+
+def pull_initial_model_to_local():
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer.save_pretrained(f"./model_store/{model_dir_name}")
+
+
+def pull_versioned_model_from_dvc():
+    pass
+
+
+def main(args=None):
+    if args.init:
+        pull_initial_model_to_local()
+    else:
+        pull_versioned_model_from_dvc()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Model Loader CLI...")
+    parser.add_argument(
+        "-i",
+        "--init",
+        action="store_true",
+        help="Pull the initial model to local",
+    )
+    parser.add_argument(
+        "-p",
+        "--pull",
+        action="store_true",
+        help="Pull the versioned model from dvc",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        type=str,
+        help="Version of the model",
+    )
+
+    args = parser.parse_args()
+    main(args)
