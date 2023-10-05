467062e87309aecec345f422afd8500e63194fb7
bbytiger <andy200212@gmail.com>
2023-10-05 14:08:46 -0400

    dvc template

--- src/model/loader.py
+++ src/model/loader.py
@@ -1,38 +1,31 @@
-# this script is not so useful anymore, since we can just directly pull from huggingface
-
-# figure out how we actually pull from dvc though
-
 import argparse
-from transformers import AutoTokenizer
-
-model_name = "tiiuae/falcon-7b"  # hardcode the model name for now
-model_dir_name = "falcon7b"
-
-
-def pull_initial_model_to_local():
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    tokenizer.save_pretrained(f"model_store/{model_dir_name}")
+import os
+import shutil
+import dvc.api
 
 
-def pull_versioned_model_from_dvc():
-    pass
+# TODO: rudra.barua figure out how to actually download models from DVC
+def pull_versioned_model_from_dvc(model_data_folder):
+    # remove and recreate the model_data folder
+    shutil.rmtree(model_data_folder, ignore_errors=True, onerror=None)
+    os.makedirs(model_data_folder, exist_ok=True)
+
+    # we will always just pull the most up to date version for now
+    dvc.api.read(
+        'model.pkl',
+        repo='https://github.com/iterative/example-get-started'
+        mode='rb'
+    )
 
 
 def main(args=None):
-    if args.init:
-        pull_initial_model_to_local()
-    else:
-        pull_versioned_model_from_dvc()
+    if args.pull:
+        model_data = "trained_model"
+        pull_versioned_model_from_dvc(model_data)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Model Loader CLI...")
-    parser.add_argument(
-        "-i",
-        "--init",
-        action="store_true",
-        help="Pull the initial model to local",
-    )
     parser.add_argument(
         "-p",
         "--pull",
