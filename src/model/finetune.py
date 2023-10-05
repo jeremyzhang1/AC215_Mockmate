80b3d7a2c335873258dbb4fd8a859ce172e4ddb1
Rudra-Barua <70864250+Rudra-Barua@users.noreply.github.com>
2023-10-04 23:02:13 -0400

    Add conversion from token tensors to text

--- src/model/finetune.py
+++ src/model/finetune.py
@@ -1,5 +1,7 @@
 import argparse
-
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import transformers
+import torch
 
 def create_model_checkpoint():
     """
@@ -10,9 +12,17 @@ def create_model_checkpoint():
     """
     pass
 
-def output_response():
-    # converting the pytorch tensors into text
-    pass
+def output_response(tensor, model_dir_name = "falcon7b"):
+    # Converting the pytorch tensors into text
+    tokenizer = AutoTokenizer.from_pretrained(f"./model_store/{model_dir_name}")
+    
+    # Convert tensor to list of token IDs
+    token_ids = tensor.tolist()
+
+    # Decode token IDs to text
+    text = tokenizer.decode(token_ids, skip_special_tokens=True)
+
+    return text
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Model Fine-tuning CLI...")
