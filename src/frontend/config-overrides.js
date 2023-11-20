ae87226b28968812126ed60eec15ec86aa7fe98a
jeremyzhang1 <zhang.jeremy.2001@gmail.com>
2023-11-20 12:21:56 -0500

    mockmate page scaffolding

---
+++ src/frontend/config-overrides.js
@@ -0,0 +1,47 @@
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const rewireBabelLoader = require("react-app-rewire-babel-loader");
+const path = require("path");
+const fs = require("fs");
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+module.exports = function override(config, env) {
+  config = rewireBabelLoader.include(
+    config,
+    resolveApp("node_modules/@monaco-editor/react")
+  );
+
+  // disable chunks so the index.html won't change during development 
+  config.optimization.splitChunks = {
+    cacheGroups: {
+      default: false,
+    },
+  };
+  config.optimization.runtimeChunk = false;
+
+  config.module.rules = [
+    {
+      test: /node_modules\/monaco-editor/,
+      use: {
+        loader: 'babel-loader',
+        // if you include your babel config here,
+        // you don’t need the `babel.config.json` file
+        options: { presets: ['@babel/preset-env'] }
+      }
+    },
+    ...config.module.rules
+  ]
+
+  config.module.rules.push({
+    test: /\.mjs$/,
+    include: /node_modules/,
+    type: "javascript/auto"
+  });
+
+  config.plugins.push(new MonacoWebpackPlugin({
+    languages: ['python']
+  }));
+
+  return config;
+}
--- src/frontend/src/pages/MockmatePage.js
+++ src/frontend/src/pages/MockmatePage.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
-import Navbar from 'react-bootstrap/Navbar'
-import Button from 'react-bootstrap/Button'
-import {Link} from 'react-router-dom'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Editor from '@monaco-editor/react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/App.css';
 
 function MockmatePage() {
     return (
-        <div>
-            <h1>Mocmkate App Page</h1>
-        </div>
+        <Container>
+            <h1>Mockmate Interview</h1>
+            <Row>
+                <Col>
+                    <p>Interview transcript</p>
+                </Col>
+                <Col>
+                    <Editor height="90vh" defaultLanguage="python" defaultValue="# your code here" theme="vs-dark"/>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
