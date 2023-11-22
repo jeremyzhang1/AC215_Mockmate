994bd1ff0e71a820441532b7ad11eceb3f60c9bf
jeremyzhang1 <zhang.jeremy.2001@gmail.com>
2023-11-21 19:41:22 -0500

    start api integration

--- src/frontend/src/pages/MockmatePage.js
+++ src/frontend/src/pages/MockmatePage.js
@@ -8,15 +8,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/App.css';
 
 function MockmatePage() {
+    const baseUrl = "http://127.0.0.1:8000" // TODO: Replace with actual production address
+    const defaultQuestion = ""
+    const defaultHelpText = "Click the button to get a hint"
+    const defaultFeedbackText = "Click the button to submit and get feedback"
     const editorRef = useRef(null);
     const [editorText, setEditorText] = useState("# your code here")
+    const [question, setQuestion] = useState(defaultQuestion)
+    const [helpText, setHelpText] = useState(defaultHelpText)
+    const [feedbackText, setFeedbackText] = useState(defaultFeedbackText)
 
     function handleEditorDidMount(editor, monaco) {
-      editorRef.current = editor;
+        editorRef.current = editor;
     }
-  
+
     function showValue() {
-      setEditorText(editorRef.current.getValue())
+        setEditorText(editorRef.current.getValue())
+        console.log(editorText)
+    }
+
+    function generateQuestion() {
+        fetch(baseUrl + '/get-interview-question')
+            .then(response => response.json())
+            .then(data => {setQuestion(data.Question)})
+            .catch(error => console.error(error));
+    }
+
+    function getHelp() {
+
+    }
+
+    function getFeedback() {
+
     }
 
     return (
@@ -24,11 +47,20 @@ function MockmatePage() {
             <h1>Mockmate Interview</h1>
             <Row>
                 <Col>
-                    <Button onClick={showValue}>Show value</Button>
+                    <h3>Question:</h3>
+                    {question == defaultQuestion ? <Button variant='dark' onClick={generateQuestion}>Start Interview</Button> : question}
+                    <br></br>
+                    <h3>Help:</h3>
+                    <Button variant='dark' onClick={getHelp}>Hint</Button>
+                    <p>{helpText}</p>
+                    <h3>Feedback:</h3>
+                    <Button variant='dark' onClick={getFeedback}>Submit</Button>
+                    <p>{feedbackText}</p>
+                    <Button variant='dark' onClick={showValue}>Show value</Button>
                     <p>{editorText}</p>
                 </Col>
                 <Col>
-                    <Editor height="90vh" defaultLanguage="python" defaultValue={editorText} theme="vs-dark" onMount={handleEditorDidMount}/>
+                    <Editor height="90vh" defaultLanguage="python" defaultValue={editorText} theme="vs-dark" onMount={handleEditorDidMount} />
                 </Col>
             </Row>
         </Container>
